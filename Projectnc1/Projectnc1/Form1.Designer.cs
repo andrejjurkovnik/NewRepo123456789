ca0415c4547c49b9493d8314ccb50d453ddd295c
Andrej <Andrej@DESKTOP-4JHHEVP>
2016-10-05 13:18:07 +0200

    b

--- Projectnc1/Projectnc1/Form1.Designer.cs
+++ Projectnc1/Projectnc1/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(120, 94);
+            this.button1.Location = new System.Drawing.Point(746, 209);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 100);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,7 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(870, 435);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
