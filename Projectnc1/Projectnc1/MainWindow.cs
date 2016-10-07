183916b9267bda2e99c9049bbf53dc19a23afc5c
Matic_PC <matic.pogacnik5@gmail.com>
2016-10-07 16:34:59 +0200

    bb

--- Projectnc1/Projectnc1/MainWindow.cs
+++ Projectnc1/Projectnc1/MainWindow.cs
@@ -69,7 +69,7 @@ namespace Projectnc1
 
             //Read all lines
             fileContent = File.ReadAllLines(@filePath);
-
+            //Briši komentar
             double[] positions;
             positions = new double[3];
             double[] ppositions = {0,0,0};
