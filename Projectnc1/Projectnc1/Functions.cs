4d30c8e07fe24032b62260fb0c9fc9fcbca50137
Matic_PC <matic.pogacnik5@gmail.com>
2016-10-07 20:59:58 +0200

    Add files

---
+++ Projectnc1/Projectnc1/Functions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Windows.Input;
+
+namespace Projectnc1
+{
+    public partial class MainWindow : Form
+    {
+
+
+
+    }
+}
